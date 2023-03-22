82b041e1c18ad9d32c2d01ccb686e46f8983c005
Satyansh Prajapati <satyanshprajapati58@gmail.com>
2023-03-22 13:59:30 +0530

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Nav from '@/components/Nav'
+import Footer from '@/components/Footer'
+import { Box } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-     
+     <Nav/>
+     <Footer/>
     </>
   )
 }
